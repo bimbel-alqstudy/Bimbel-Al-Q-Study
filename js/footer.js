bd55750199ff9ce5a2de01591c9893aefd8c0ce0
bimbel-alqstudy <bimbel.alqstudy@gmail.com>
2026-03-16 15:23:00 +0700

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -12,31 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="footer-motto">
           <h3>Bimbingan Belajar</h3>
           <h2>Al-Q Study</h2>
-          <p>Belajar menyenangkan, Prestasi Gemilang!</p>
+          <p>Jl. Bareng Tengah 5C No. 781 B Kota Malang</p>
         </div>
       </div>
 
       <!-- Kanan: Kontak -->
       <div class="footer-contact">
         <h4>Hubungi Kami</h4>
-
         <ul>
-          <li>
-            <i class="fas fa-envelope"></i>
-            <a href="mailto:joko.shabieq@gmail.com">
-              joko.shabieq@gmail.com
-            </a>
-          </li>
-          <li>
-            <i class="fab fa-instagram"></i>
-            <a href="https://instagram.com/j0k0_cahy0n0" target="_blank" rel="noopener">
-              @j0k0_cahy0n0
-            </a>
-          </li>
           <li>
             <i class="fab fa-whatsapp"></i>
             <a href="https://wa.me/6285604606502" target="_blank" rel="noopener">
               085604606502
+            </a>
+          </li>
+          <li>
+            <i class="fab fa-instagram"></i>
+            <a href="https://instagram.com/bimbel.alqstudy" target="_blank" rel="noopener">
+              @bimbel.alqstudy
+            </a>
+          </li>
+        <li>
+          <i class="fab fa-tiktok"></i>
+          <a href="https://tiktok.com/@bimbel.alqstudy" target="_blank" rel="noopener">
+          @bimbel.alqstudy
+          </a>
+        </li>
+          <li>
+            <i class="fas fa-envelope"></i>
+            <a href="mailto:bimbel.alqstudy@gmail.com">
+              bimbel.alqstudy@gmail.com
             </a>
           </li>
         </ul>
