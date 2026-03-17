47bacfbdf817223119d339a08dc07d8577d192fb
bimbel-alqstudy <bimbel.alqstudy@gmail.com>
2026-03-17 21:30:27 +0700

    Update kelas.js

--- js/kelas.js
+++ js/kelas.js
@@ -1,12 +1,19 @@
 // ===== AMBIL PARAMETER KELAS =====
 const params = new URLSearchParams(window.location.search);
 const kelasAktif = parseInt(params.get("kelas"));
-console.log("kelasAktif =", kelasAktif);
 
 // ===== ELEMEN DOM =====
 const judulKelas = document.getElementById("judulKelas");
+const deskripsiKelas = document.getElementById("deskripsiKelas");
+const materiKelas = document.getElementById("materiKelas");
+const latihanKelas = document.getElementById("latihanKelas");
+const tryoutKelas = document.getElementById("tryoutKelas");
 
 
 // ===== INIT TEKS HALAMAN =====
-judulKelas.textContent = kelasAktif ? `Sekolah Dasar (SD) Kelas ${kelasAktif}` : "Kelas";
+judulKelas.textContent = kelasAktif ? `Kelas ${kelasAktif}` : "Kelas";
+deskripsiKelas.textContent = kelasAktif ? `Kumpulan materi pelajaran, bank soal, dan latihan online untuk kelas ${kelasAktif}.` : "Kumpulan materi pelajaran, bank soal, dan latihan online.";
+materiKelas.textContent = kelasAktif ? `Kumpulan materi pelajaran untuk kelas ${kelasAktif}` : "Kumpulan materi pelajaran";
+latihanKelas.textContent = kelasAktif ? `Bank soal untuk kelas ${kelasAktif}` : "Bank soal";
+tryoutKelas.textContent = kelasAktif ? `Latihan online untuk kelas ${kelasAktif}` : "Latihan online";
 
